171a5877d62366a891d3a98400a821454333b3c1
xiaoke799 <100974160@qq.com>
2026-09-23 09:59:22 +0800

    chore: remove third-party fnOS docs, fnpack binaries and stale release artifacts

--- 飞牛开发文档及脚手架/fnos-app-template-main/fnos-app-template-main/packages/server/routes/api/session.get.ts
+++
@@ -1,5 +0,0 @@
-import { defineEventHandler } from "h3";
-import { ok } from "../../utils/api-response";
-import { getGatewayUser } from "../../utils/gateway-user";
-
-export default defineEventHandler((event) => ok(getGatewayUser(event)));
--- 飞牛开发文档及脚手架/fnos-app-template-main/fnos-app-template-main/packages/server/routes/api/system.get.ts
+++
@@ -1,5 +0,0 @@
-import { defineEventHandler } from "h3";
-import { ok } from "../../utils/api-response";
-import { getSystemSummary } from "../../services/system.service";
-
-export default defineEventHandler(() => ok(getSystemSummary()));
